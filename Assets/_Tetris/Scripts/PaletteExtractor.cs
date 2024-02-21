6ac80e4ff5718992389bc99135c0430b832f8c2d
Feddas <feddas00@gmail.com>
2024-02-20 23:34:10 -0800

    PaletteExtractor.cs copied from https://gist.github.com/vicot/0c5e9839bebba9436dfc543eda790bd1

---
+++ Assets/_Tetris/Scripts/PaletteExtractor.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PaletteExtractor : ScriptableObject
+{
+    [SerializeField] private ScriptableObject palette;
+
+    [field:SerializeField] public List<ColorPreset> ColorPresets { get; set; } = new();
+
+    [ContextMenu("Read palettes")]
+    private void Fix()
+    {
+        if (palette == null) return;
+
+        var type = palette.GetType();
+        if (type.Name != "ColorPresetLibrary")
+        {
+            Debug.LogWarning($"'{type.Name}' is not a color Palette");
+            return;
+        }
+
+        var presetsField = type.GetField("m_Presets", BindingFlags.Instance | BindingFlags.NonPublic);
+        if (presetsField == null)
+        {
+            Debug.LogWarning("ColorPresetLibrary implementation changed, fix the script");
+            return;
+        }
+
+        if (presetsField.GetValue(palette) is not IList presets || presets.Count == 0) return;
+
+        var presetType       = presets[0].GetType();
+        var presetColorField = presetType.GetProperty("color", BindingFlags.Instance | BindingFlags.Public);
+        var presetNameField  = presetType.GetProperty("name", BindingFlags.Instance | BindingFlags.Public);
+
+        if (presetColorField == null || presetNameField == null)
+        {
+            Debug.LogWarning("ColorPreset implementation changed, fix the script");
+            return;
+        }
+
+        ColorPresets.Clear();
+        foreach (var preset in presets)
+        {
+            var color = (Color)presetColorField.GetValue(preset);
+            var name  = (string)presetNameField.GetValue(preset);
+            ColorPresets.Add(new ColorPreset(color, name));
+        }
+    }
+
+    private void OnValidate()
+    {
+        Fix();
+    }
+
+    [Serializable]
+    public class ColorPreset
+    {
+        [field: SerializeField] public Color Color { get; set; }
+        [field: SerializeField] public string Name { get; set; }
+
+        public ColorPreset(Color color, string name)
+        {
+            Color = color;
+            Name  = name;
+        }
+    }
+}
